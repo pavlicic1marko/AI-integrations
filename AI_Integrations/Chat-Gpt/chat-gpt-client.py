43f22fbe7b172c0eb956bf0c89ff4edbaaa0e828
MP <pavlicic1marko@gmail.com>
2025-01-01 20:09:32 +0100

    refactor

--- AI_Integrations/Chat-Gpt/chat-gpt-client.py
+++ AI_Integrations/Chat-Gpt/chat-gpt-client.py
@@ -1,7 +1,27 @@
 from dotenv import load_dotenv
 import os
+import openai
+
 load_dotenv()
 
+api_key = os.getenv("chat_gpt_api")
+openai.api_key = api_key
 
-SBR_WEBDRIVER = os.getenv("chat_gpt_api")
-print(SBR_WEBDRIVER)
+# model options
+gpt_4 = "gpt-4"
+gpt_3x5_turbo = "gpt-3.5-turbo"
+
+
+def ask_chat_gpt_a_question(question):
+    model_id = gpt_4
+    # Initial message to ask the first question
+    messages = [{"role": "user", "content": question}]
+
+    # Call the API
+    completion = openai.chat.completions.create(model=model_id, messages=messages)
+
+    # Print the answer we received for the 1st question
+    print(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    ask_chat_gpt_a_question("is Serbia in the EU")
