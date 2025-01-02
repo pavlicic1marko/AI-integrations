6a23af0d3042f902d3febda512adf3cf40c2dd17
MP <pavlicic1marko@gmail.com>
2025-01-02 13:55:34 +0100

    adding chat-gpt client

---
+++ AI_Integrations/djangoWebapp/Chat_Gpt/chat-gpt-client.py
@@ -0,0 +1,26 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+#load and set api_key for open ai
+load_dotenv()
+api_key = os.getenv("chat_gpt_api_key")
+openai.api_key = api_key
+
+# model options
+gpt_4 = "gpt-4"
+gpt_3x5_turbo = "gpt-3.5-turbo"
+model_id = gpt_4
+
+def ask_chat_gpt(question):
+    # Initial message to ask the first question
+    messages = [{"role": "user", "content": question}]
+
+    # Call the API
+    completion = openai.chat.completions.create(model=model_id, messages=messages)
+
+    # Print the answer we received for the 1st question
+    print(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    ask_chat_gpt("is serbia a member of the eu")
