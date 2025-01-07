270a3dfb0e97eaa87e553990b51803358daa2256
MP <pavlicic1marko@gmail.com>
2025-01-07 18:47:02 +0100

    chat-gpt client on the FE

--- AI_Integrations/djangoWebapp/ChatGpt/client.py
+++ AI_Integrations/djangoWebapp/ChatGpt/client.py
@@ -1,19 +1,18 @@
-from dotenv import load_dotenv
 import os
 import openai
+from dotenv import load_dotenv
 
+#load and set api_key for open ai
 load_dotenv()
-
-api_key = os.getenv("chat_gpt_api")
+api_key = os.getenv("chat_gpt_api_key")
 openai.api_key = api_key
 
 # model options
 gpt_4 = "gpt-4"
 gpt_3x5_turbo = "gpt-3.5-turbo"
+model_id = gpt_4
 
-
-def ask_chat_gpt_a_question(question):
-    model_id = gpt_4
+def ask_chat_gpt(question):
     # Initial message to ask the first question
     messages = [{"role": "user", "content": question}]
 
@@ -21,7 +20,8 @@ def ask_chat_gpt_a_question(question):
     completion = openai.chat.completions.create(model=model_id, messages=messages)
 
     # Print the answer we received for the 1st question
-    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
+
 
 if __name__ == "__main__":
-    ask_chat_gpt_a_question("is Serbia in the EU")
+    ask_chat_gpt("is serbia a member of the eu")
