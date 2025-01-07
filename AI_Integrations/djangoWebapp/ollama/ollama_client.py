6de3e02c0c3d3de097e67f0c8eee51b6fc214ef1
MP <pavlicic1marko@gmail.com>
2025-01-07 21:56:42 +0100

    refactor

--- AI_Integrations/djangoWebapp/ollama/ollama_client.py
+++ AI_Integrations/djangoWebapp/ollama/ollama_client.py
@@ -8,6 +8,5 @@ response: ChatResponse = chat(model='llama3.2:1b', messages=[
     'content': 'is serbia in the eu',
   },
 ])
-print(response['message']['content'])
-# or access fields directly from the response object
+# access fields directly from the response object
 print(response.message.content)
