c19a1d64858c959d1e7bfeece94023d9d6e4e508
MP <pavlicic1marko@gmail.com>
2025-01-10 16:56:01 +0100

    create FE ollama-prompt page, configure urls and views

--- AI_Integrations/djangoWebapp/views.py
+++ AI_Integrations/djangoWebapp/views.py
@@ -6,15 +6,18 @@ from .ChatGpt.client import ask_chat_gpt
 from .forms import SignUpForm
 from .models import ChatGptPrompts
 
-chat_history =[]
+chat_history = []
+
 
 def home(reqeuest):
     return render(reqeuest, 'authenticate/home.html', {})
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, ('You Have Been Logged Out...'))
     return redirect('home')
+
 
 def login_user(request):
     if request.method == 'POST':
@@ -23,13 +26,13 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request,('You Have been logged in!'))
+            messages.success(request, ('You Have been logged in!'))
             return redirect('home')
         else:
-            messages.error(request,('Error'))
+            messages.error(request, ('Error'))
             return redirect('login')
     else:
-        return render(request, 'authenticate/login.html',{})
+        return render(request, 'authenticate/login.html', {})
 
 
 def register_user(request):
@@ -46,8 +49,9 @@ def register_user(request):
     else:
         form = SignUpForm()
 
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'authenticate/register.html', context)
+
 
 def chat_gpt_prompt_page(request):
     if request.user.is_authenticated:
@@ -55,15 +59,31 @@ def chat_gpt_prompt_page(request):
             question = request.POST["question"]
             answer = ask_chat_gpt(question)
 
-            chat_gpt_prompt = ChatGptPrompts(question=question, answer=answer, user=request.user )
+            chat_gpt_prompt = ChatGptPrompts(question=question, answer=answer, user=request.user)
             chat_gpt_prompt.save()  # Save to the database
 
-
-
-            chat_history.append({'question':question,'answer':answer})
+            chat_history.append({'question': question, 'answer': answer})
             return render(request, 'authenticate/prompt.html', {'questions': chat_history})
         else:
             return render(request, 'authenticate/prompt.html')
     else:
         return render(request, 'authenticate/home.html')
 
+
+def ollama_prompt_page(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            question = request.POST["question"]
+            answer = 'test'
+
+            chat_gpt_prompt = ChatGptPrompts(question=question, answer=answer, user=request.user)
+            chat_gpt_prompt.save()  # Save to the database
+
+            chat_history_llama = [{'question': question, 'answer': answer}]
+
+            return render(request, 'authenticate/ollama-prompt.html', {'questions': chat_history_llama})
+        else:
+            return render(request, 'authenticate/ollama-prompt.html')
+
+    else:
+        return render(request, 'authenticate/home.html')
