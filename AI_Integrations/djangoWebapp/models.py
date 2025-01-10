6be06586ff7e3bf2bfc36fe7ffc65c3f60f92908
MP <pavlicic1marko@gmail.com>
2025-01-10 11:06:51 +0100

    adding prompts model

--- AI_Integrations/djangoWebapp/models.py
+++ AI_Integrations/djangoWebapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ChatGptPrompts(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.item
