6a909bbb2fe26d19bdf2e69bf950206239021310
MP <pavlicic1marko@gmail.com>
2025-01-10 12:13:05 +0100

    add foreign key and add migrations file

--- AI_Integrations/djangoWebapp/models.py
+++ AI_Integrations/djangoWebapp/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class ChatGptPrompts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ChatGptPrompts')
     question = models.CharField(max_length=200)
     answer = models.CharField(max_length=200)
 
