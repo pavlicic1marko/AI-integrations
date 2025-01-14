c3c0993681e71e3cc84da2c0cb4d46e7fd760352
MP <pavlicic1marko@gmail.com>
2025-01-14 17:09:39 +0100

    show aiModel on FE, edit db model, save model info after making a prompt

--- AI_Integrations/djangoWebapp/models.py
+++ AI_Integrations/djangoWebapp/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class ChatGptPrompts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ChatGptPrompts')
     question = models.CharField(max_length=200)
     answer = models.CharField(max_length=200)
-
+    aiModel = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question
