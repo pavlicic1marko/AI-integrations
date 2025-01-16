6c6a1344d431363e3e02813e35c724e9a2c17c81
MP <pavlicic1marko@gmail.com>
2025-01-16 18:23:09 +0100

    set url path for images

--- AI_Integrations/djangoWebapp/urls.py
+++ AI_Integrations/djangoWebapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -13,4 +16,4 @@ urlpatterns = [
     path('change_password/', views.change_password, name='change_password'),
     path('llava-prompt/', views.llava_promp_page, name='llava-prompt'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
