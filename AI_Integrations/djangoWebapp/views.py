d2761f7c3b503eb023728afb89ade6cf019d77c5
MP <pavlicic1marko@gmail.com>
2025-01-16 16:03:32 +0100

    adding llava prompt page

--- AI_Integrations/djangoWebapp/views.py
+++ AI_Integrations/djangoWebapp/views.py
@@ -123,3 +123,16 @@ def change_password(request):
 
     context = {'form': form}
     return render(request, 'authenticate/change_password.html', context)
+
+def llava_promp_page(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            return render(request, 'authenticate/llava-prompt.html', )
+
+        else:
+            return render(request, 'authenticate/llava-prompt.html')
+    else:
+        return render(request, 'authenticate/home.html')
+
+
+
